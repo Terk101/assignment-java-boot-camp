041b3a084acac47d5825e1914a3117922bb17bc6
terk101 <tantaizaza@gmail.com>
2022-02-19 20:25:27 +0700

    #2 setup project.

---
+++ assignment/src/test/java/com/tantai/assignment/JavaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tantai.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
